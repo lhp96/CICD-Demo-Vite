4e68ee8ee953589485df670f2af3856e81dcee27
luhuipeng <973880264@qq.com>
2022-10-16 16:45:27 +0800

    feat: test-CD

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import type { FC } from "react";
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import type { FC } from 'react';
+import { useState } from 'react';
+import './App.css';
+import logo from './logo.svg';
 
 interface Props {
   value: string;
@@ -20,14 +20,12 @@ const App: FC<Props> = ({ value }) => {
             测试代码中需要获取的DOM元素用role属性标记，这里的role属性只会在测试代码中用到，
             这样子就可以避免代码因需求改动时，因DOM属性改变导致测试不通过。有利于TDD（测试驱动开发）开发的进行
           */}
-          <button
-            role="button"
-            type="button"
-            onClick={() => setCount((v) => v + 1)}
-          >
+          <button role="button" type="button" onClick={() => setCount((v) => v + 1)}>
             count is: {count}
           </button>
         </p>
+
+        <h1>2022.10.16--16:44</h1>
         <p role="props">{value}</p>
       </header>
     </div>
